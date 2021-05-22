ca91f87300b28cf138cef033fa059cb060467bdd
Vincent Herlemont <vincentherl@leszeros.com>
2021-05-22 22:36:22 +0000

    Remove script directory to build.

--- script/build.sh
+++ script/build.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd $SCRIPT_DIR/../
 
 zip -r save-to-notion-$(cat manifest.json | jq .version -r).zip . \
-    -x '*.idea*' -x '*.blanck*' -x '*.git*'
+    -x '*.idea*' -x '*.blanck*' -x '*.git*' -x 'script/*'
 
 popd
